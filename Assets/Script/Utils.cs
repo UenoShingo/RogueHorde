db48478d7cc0b7570215b9bcaed98bc9aef47baa
Ueno_Shingo <ueno_shingo2002@yahoo.co.jp>
2026-06-22 03:51:19 +0900

    ゲームタイマー表示を実装

---
+++ Assets/Script/Utils.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲームで使う共通処理をまとめた便利クラス
+public static class Utils
+{
+    // 秒数を0:00の文字列に変換
+    public static string GetTextTimer(float timer)
+    {
+        int seconds = (int)timer % 60;
+        int minutes = (int)timer / 60;
+        return minutes.ToString() + ":" + seconds.ToString("00");
+    }
+}
